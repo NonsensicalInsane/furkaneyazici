e58bd31ae3a05f93d48815c05c83e6caa0d4c24a
Furkan Eşref Yazıcı <yazici.furkan@proton.me>
2024-03-10 01:32:56 +0300

    Updating

--- src/navigation.js
+++ src/navigation.js
@@ -7,13 +7,14 @@ export const headerData = {
 export const footerData = {
   
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://twitter.com/furkaneyazici' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://instagram.com/nonsensicalinsane' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/nonsensicalinsane' },
-    { ariaLabel: 'Linkedin', icon: 'tabler:brand-linkedin', href: 'https://linkedin.com/in/nonsensicalinsane' },
-    { ariaLabel: 'Mail', icon: 'tabler:mail', href: 'mailto:yazici.furkan@proton.me'},
-    { ariaLabel: 'Upwork', icon: 'tabler:brand-upwork', href: 'https://www.upwork.com/freelancers/~01ae41556331664a51' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://twitter.com/furkaneyazici', target:"_blank" },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://instagram.com/nonsensicalinsane' , target:"_blank"},
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') , target:"_blank"},
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/nonsensicalinsane' , target:"_blank"},
+    { ariaLabel: 'Linkedin', icon: 'tabler:brand-linkedin', href: 'https://linkedin.com/in/nonsensicalinsane' , target:"_blank"},
+    { ariaLabel: 'Email', icon: 'tabler:mail', href: 'mailto:yazici.furkan@proton.me', target:"_blank"},
+    { ariaLabel: 'Upwork', icon: 'tabler:brand-upwork', href: 'https://www.upwork.com/freelancers/~01ae41556331664a51', target:"_blank" },
+    { ariaLabel: 'Youtube', icon: 'tabler:brand-youtube', href: 'https://www.youtube.com/@furkaneyazici', target:"_blank"}
   ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm"><img src="/src/assets/images/icons/logo.ico"/></span>

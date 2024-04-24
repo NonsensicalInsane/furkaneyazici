11ab1f33f8ce30d87843b886bcb82c09193e2437
Furkan Yazıcı <yazici.furkan@proton.me>
2024-04-24 16:15:29 +0300

    updating

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,15 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
-import compress from 'astro-compress';
+import compress from '@playform/compress';
 import tasks from './src/utils/tasks';
 import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin } from './src/utils/frontmatter.mjs';
 import { SITE } from './src/utils/config.ts';
 import vercel from "@astrojs/vercel/static";
 import preact from "@astrojs/preact";
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 
@@ -52,8 +54,8 @@ export default defineConfig({
     service: squooshImageService()
   },
   markdown: {
-    remarkPlugins: [readingTimeRemarkPlugin],
-    rehypePlugins: [responsiveTablesRehypePlugin],
+    remarkPlugins: [readingTimeRemarkPlugin, remarkMath],
+    rehypePlugins: [responsiveTablesRehypePlugin, rehypeKatex],
     
   },
   vite: {
